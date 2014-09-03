69c9cb7d09c76f47e4120f5ef93a6b82d021410b
Joshua Robbins <jrobbin@sandia.gov>
2014-09-03 07:08:17 -0600

    minor fix to added tests

--- examples/ATO/MultiLoadSet/runtest.cmake
+++ examples/ATO/MultiLoadSet/runtest.cmake
@@ -22,22 +22,4 @@ if(HAD_ERROR)
 endif()
 
 EXECUTE_PROCESS(COMMAND ${TEST_PROG} element_nlopt.xml RESULT_VARIABLE HAD_ERROR)
-
-if(HAD_ERROR)
-	message(FATAL_ERROR "Albany didn't run: test failed")
-endif()
-
-if(HAD_ERROR)
-	message(FATAL_ERROR "Test failed")
-endif()
-
-
 EXECUTE_PROCESS(COMMAND ${TEST_PROG} nodal_nlopt.xml RESULT_VARIABLE HAD_ERROR)
-
-if(HAD_ERROR)
-	message(FATAL_ERROR "Albany didn't run: test failed")
-endif()
-
-if(HAD_ERROR)
-	message(FATAL_ERROR "Test failed")
-endif()
