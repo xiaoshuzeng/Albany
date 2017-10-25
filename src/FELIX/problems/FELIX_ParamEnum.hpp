9ac5a167b25a9000169413fd46e2b3607ff2e144
Luca Bertagna <lbertag@sandia.gov>
2017-10-25 09:56:07 -0600

    Added variables to store FELIX parameter names in FELIX_ParamEnum.hpp
    This is to reduce the number of places where the parameter name is
    hard-coded. One should try as much as possible to use the XYZ_name string
    for parameter XYZ, so that the chance of having wrong parameter names (typos)
    is reduced.

--- src/FELIX/problems/FELIX_ParamEnum.hpp
+++ src/FELIX/problems/FELIX_ParamEnum.hpp
@@ -1,6 +1,8 @@
 #ifndef FELIX_PARAM_ENUM_HPP
 #define FELIX_PARAM_ENUM_HPP
 
+#include <string>
+
 namespace FELIX
 {
 
@@ -11,10 +13,18 @@ enum class FelixParamEnum
   Mu           = 2,
   Power        = 3,
   Homotopy     = 4,
-  FlowFactorA  = 5,
-  FieldNormReg = 6
+  FlowFactorA  = 5
 };
 
+namespace ParamEnum
+{
+  static const std::string Alpha_name         = "Hydraulic-Over-Hydrostatic Potential Ratio";
+  static const std::string Lambda_name        = "Bed Roughness";
+  static const std::string Mu_name            = "Coulomb Friction Coefficient";
+  static const std::string Power_name         = "Power Exponent";
+  static const std::string HomotopyParam_name = "Homotopy Parameter";
+  static const std::string FlowFactorA_name   = "Constant Flow Factor A";
+} // ParamEnum
 
 } // Namespace FELIX
 
